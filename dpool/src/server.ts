e4d0b2a59a60f9075ee803ce92362e62f61e678e
Yin Qu <me.yin.qu@gmail.com>
2016-02-23 23:22:23 -0600

    dpool: better integration w existing BS service

--- dpool/src/server.ts
+++ dpool/src/server.ts
@@ -68,6 +68,76 @@ DownloaderPool.create((err, dpool) => {
     });
   });
 
+  app.get('/DownloaderPool/echo/get', (req, resp) => {
+    var msg = req.query.msg;
+    resp.send('Echo Message: ' + msg);
+    resp.end();
+  });
+
+  app.get('/DownloaderPool/page/download.json', (req, resp) => {
+    if (!req.query.url || req.query.url === '') {
+      resp.status(400);
+      resp.end("Parameter 'url' is required.");
+      return;
+    }
+
+    dpool.newTask(toTaskProto(req.query), (err, task) => {
+      if (err) {
+        resp.status(500);
+        resp.json(task);
+        resp.end();
+        return;
+      }
+
+      // generate JSON response
+      if (task.response.raw) {
+        task.response.content = task.response.raw.toString();
+        delete task.response.raw;
+      }
+      resp.type('json');
+      resp.send({
+        download_task: {
+          id: task.id,
+          state: task.state === 'finished' ? 'SUCCEEDED' : 'TERMINATED',
+          user_agent: task.userAgent,
+          max_attempts: task.maxAttempts,
+          attempt_time: task.msPerAttempt,
+
+          url: req.query.url,
+
+          response: {
+            url: task.response.url,
+            code: task.response.code,
+            other_urls: (!task.response.otherUrls) ? [] : task.response.otherUrls.map(otherUrl => {
+              return {
+                other_url: otherUrl,
+              };
+            }),
+            headers: (!task.response.headers) ? [] : task.response.headers.map(hdr => {
+              return {
+                name: hdr.name,
+                value: hdr.value,
+              };
+            }),
+            content: task.response.content,
+          }, // response
+
+          // we have to skip logs for now, to not break JSON deserializer in
+          // Java.
+          /*
+          logs: (!task.logs) ? [] : task.logs.map(log => {
+            return {
+              datetime: log.datetime,
+              name: log.name,
+              args: JSON.stringify(log.args),
+            };
+          }),
+          */
+        }, // download_task
+      }); // resp.send()
+    }); // dpool.newTask() callback
+  }); // app.get() callback
+
   app.get('/DownloaderPool/page/download.xml', (req, resp) => {
     if (!req.query.url || req.query.url === '') {
       resp.status(400);
