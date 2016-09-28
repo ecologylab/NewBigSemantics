8c89a82f037878d659ce134206e483ab7ed92c86
Yin Qu <me.yin.qu@gmail.com>
2016-09-28 17:05:21 -0500

    Updated BSJS. Made BS and dpool service load configurations. Configurations can spread in a required default file (source controlled) and a set of instance-specific files (not source controlled).

--- src/bscore/server.ts
+++ src/bscore/server.ts
@@ -1,50 +1,76 @@
 // Entry point for BigSemanticsService
 
+import * as fs from 'fs';
+import * as http from 'http';
+import * as https from 'https';
 import * as express from 'express';
+
+import logger from './logging';
+import * as config from '../utils/config';
+
 import * as bsservice from './middleware';
 import * as dashboard from '../dashboard/middleware';
 
-const PORT = process.env.BS_SERVICE_PORT || 8000;
+var conf: any = config.get('service');
+if (conf == null) {
+  logger.fatal("Failed to load configurations");
+} else if (conf instanceof Error) {
+  logger.fatal({ err: conf as Error }, "Error loading configurations");
+} else {
+  logger.info("Configurations loaded: %s", conf);
 
-var app = express();
-var bsRouter = express.Router();
-bsservice.create((err, res) => {
-  if (err) {
-    console.error(err);
-    return;
+  var bsRouter = express.Router();
+
+  bsservice.create((err, res) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    bsRouter.use("/metadata.json", res.metadataJson);
+    bsRouter.use("/metadata.jsonp", res.metadataJsonp);
+
+    bsRouter.use("/wrapper.json", res.wrapperJson);
+    bsRouter.use("/wrapper.jsonp", res.wrapperJsonp);
+    bsRouter.use("/mmd.json", res.wrapperJson);
+    bsRouter.use("/mmd.jsonp", res.wrapperJsonp);
+
+    bsRouter.use("/repository.json", res.repositoryJson);
+    bsRouter.use("/repository.jsonp", res.repositoryJsonp);
+    bsRouter.use("/mmdrepository.json", res.repositoryJson);
+    bsRouter.use("/mmdrepository.jsonp", res.repositoryJsonp);
+
+    bsRouter.use("/tasks.json", res.tasksJson);
+    bsRouter.use("/task.json", res.taskJson);
+
+    bsRouter.use("/agents.json", res.agentsInfoJson);
+    bsRouter.use("/downloaders.json", res.downloadersInfoJson);
+
+    bsRouter.use(res.errorHandler);
+  });
+
+  dashboard.create((err, res) => {
+    if(err) {
+      console.error(err);
+      return;
+    }
+
+    bsRouter.use("/dashboard", res.index);
+  });
+
+  var app = express();
+  app.use("/BigSemanticsService", bsRouter);
+  var httpServer = http.createServer(app);
+  httpServer.listen(conf.port);
+
+  if (conf.use_https) {
+    var options = {
+      passphrase: conf.passphrase,
+      pfx: fs.readFileSync(conf.pfx_path),
+    };
+    var httpsServer = https.createServer(options, app);
+    httpsServer.listen(conf.secure_port);
   }
 
-  bsRouter.use("/metadata.json", res.metadataJson);
-  bsRouter.use("/metadata.jsonp", res.metadataJsonp);
-
-  bsRouter.use("/wrapper.json", res.wrapperJson);
-  bsRouter.use("/wrapper.jsonp", res.wrapperJsonp);
-  bsRouter.use("/mmd.json", res.wrapperJson);
-  bsRouter.use("/mmd.jsonp", res.wrapperJsonp);
-  
-  bsRouter.use("/repository.json", res.repositoryJson);
-  bsRouter.use("/repository.jsonp", res.repositoryJsonp);
-  bsRouter.use("/mmdrepository.json", res.repositoryJson);
-  bsRouter.use("/mmdrepository.jsonp", res.repositoryJsonp);
-
-  bsRouter.use("/tasks.json", res.tasksJson);
-  bsRouter.use("/task.json", res.taskJson);
-  
-  bsRouter.use("/agents.json", res.agentsInfoJson);
-  bsRouter.use("/downloaders.json", res.downloadersInfoJson);
-
-  bsRouter.use(res.errorHandler);
-});
-
-dashboard.create((err, res) => {
-  if(err) {
-    console.error(err);
-    return;
-  }
- 
-  bsRouter.use("/dashboard", res.index);
-});
-
-app.use("/BigSemanticsService", bsRouter);
-app.listen(PORT);
-console.log("Service running on port " + PORT);
+  console.log("BigSemantics Service running on port " + conf.port + (conf.use_https ? (" and " + conf.secure_port) : ""));
+}
